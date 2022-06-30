eddcdcf8355a2b5a219f9397d338bf18fbfe08c2
Amir Nourinia <amir.nourinia@tum.de>
2022-06-30 04:14:38 +0200

    Added auto color switch + navigation links and navigation view

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -9,22 +9,43 @@ import SwiftUI
 
 struct ScrumsView: View {
     
+    @Environment(\.colorScheme)
+    private var colorScheme
+    
+    private var invColorScheme: Color? {
+        switch colorScheme {
+        case .dark:
+            return Color.white
+        case .light:
+            return Color.black
+        default:
+            print("Unknown color scheme!")
+            return .none
+        }
+    }
+    
     let scrums: [DailyScrum]
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                CardView(scrum: scrum)
-                    .listRowBackground(scrum.theme.mainColor)
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }.listRowBackground(scrum.theme.mainColor)
             }
         }
+        .navigationTitle("Daily Scrums").foregroundColor(invColorScheme!)
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: DailyScrum.sampleData)
-            .colorScheme(.dark)
-        ScrumsView(scrums: DailyScrum.sampleData)
-            .colorScheme(.light)
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.sampleData)
+                .colorScheme(.dark)
+        }
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.sampleData)
+                .colorScheme(.light)
+        }
     }
 }
